bbabfa71fee0482823be434250cb95d147a9da22
FacuGomez192497 <89607690+1924GAF97@users.noreply.github.com>
2022-08-24 14:45:39 -0300

    segunda parte completa

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Pages from "./pages/Pages";
 import Category from "./components/Category";
+import { BrowserRouter } from "react-router-dom";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
-      <Category/>
-      <Pages/>
+      <BrowserRouter>
+        <Search/>
+        <Category/>
+        <Pages/>
+      </BrowserRouter>
     </div>
   );
 }
